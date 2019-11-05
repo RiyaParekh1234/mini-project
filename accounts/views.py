3f0abe569b06a92eaf420cee51cb0f57e1ba6734
Riya Parekh <parekhriya15@gmail.com>
2019-11-05 17:25:52 +0530

    appoint date done

--- accounts/views.py
+++ accounts/views.py
@@ -115,7 +115,7 @@ def p_next(request):
         med = request.POST['med']
         pas1 = request.POST['pass1']
         pas2 = request.POST['pass2']
-        user = request.session["usr"]
+        user = request.session["mail"]
         query1 = 'insert into patient(id,ht,wt,med_history) values ((select id from person where emailid="'+user+'"),"' + ht + '","' + wt + '","' + med + '")'
         query2 = "update person set pswd1 ='"+pas1+"',pswd2 = '"+pas2+"' where emailid = '" + request.session["email"] + "' "  
         
